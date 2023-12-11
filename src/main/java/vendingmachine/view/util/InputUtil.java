a801767b1beab35488dbff4144f90b3fafc43a66
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-11 20:42:04 +0900

    feat: add input utils

--- src/main/java/vendingmachine/view/util/InputUtil.java
+++ src/main/java/vendingmachine/view/util/InputUtil.java
@@ -1,6 +1,28 @@
 package vendingmachine.view.util;
 
+import java.util.function.Supplier;
+import vendingmachine.exception.ErrorMessage;
+import vendingmachine.exception.InvalidInputException;
+
 public class InputUtil {
     private InputUtil() {
+    }
+
+    public static int parseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException(ErrorMessage.INPUT_NOT_A_NUMBER);
+        }
+    }
+
+    public static <T> T retryOnException(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
